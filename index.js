2bf82e1fa01453f427dd6638bd53ec9796ecc170
Jesse McIntire <jessemcintire2@gmail.com>
2019-04-19 14:02:08 -0400

    setup basic html and start js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-alert('connected');
+const calcDisplay = document.querySelector('.calc-display');
+let displayText = '';
+const keypad = document.querySelector('.keypad');
+const numButtons = Array.from(document.querySelectorAll('.numButton'));
+
+numButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    displayText = `${this.getAttribute('data-value')}`;
+    calcDisplay.textContent += displayText;
+  });
+});
